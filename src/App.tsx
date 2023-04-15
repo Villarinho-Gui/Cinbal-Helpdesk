9a1129310cdcb2927b4d868c56c6a57f4514c564
Guilherme Villarinho <Guivillarinho15@gmail.com>
2023-04-15 07:36:42 -0300

    feat(app.tsx): :sparkles: ThemeProvider
    Encapsulamento de todos os componentes para terem o tema definido.

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
 
+import { ThemeProvider } from "@mui/material";
+import { LightTheme } from "./shared/themes";
+
 export const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
