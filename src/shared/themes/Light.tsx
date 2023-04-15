c6cd281b1cf9315568b6cfb08cd470925817ef93
Guilherme Villarinho <Guivillarinho15@gmail.com>
2023-04-15 07:33:11 -0300

    feat(src\shared\themes\light.tsx): :sparkles: Criação do tema Light
    Configuração do tema light do material ui

---
+++ src/shared/themes/Light.tsx
@@ -0,0 +1,25 @@
+import { createTheme } from "@mui/material";
+import { blue, grey } from "@mui/material/colors";
+
+export const LightTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#3A89E6",
+      dark: "#4D84E3",
+      light: blue[500],
+
+      contrastText: "#ffffff",
+    },
+    secondary: {
+      main: grey[500],
+      dark: grey[400],
+      light: grey[300],
+
+      contrastText: "#ffffff",
+    },
+    background: {
+      default: "#FBFBFB",
+      paper: "#F5F5F5",
+    },
+  },
+});
