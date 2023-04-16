e6d6c38e1426660ddc0a545eb3955dcf53dae963
Guilherme Villarinho <Guivillarinho15@gmail.com>
2023-04-16 07:20:27 -0300

    feat(src\routes\index.tsx): :sparkles: Adicionando contexto na aplicação
    Aqui estamos recebendo o contexto que foi criado lá no AppThemeContext e passando pelo
    useAppThemeContext() que foi criado como um hook próprio para poder utilizar a função que foi criada
    lá.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,15 +2,19 @@ import { Button } from "@mui/material";
 import React from "react";
 
 import { Route, Routes, Navigate } from "react-router-dom";
+import { useAppThemeContext } from "../shared/contexts";
 
 export const AppRoutes: React.FC = () => {
+
+const { toggleTheme } = useAppThemeContext();
+
   return (
     <Routes>
       <Route
         path="/home"
         element={
-          <Button color="primary" variant="contained">
-            Teste
+          <Button color="primary" variant="contained" onClick={toggleTheme}>
+            Tema
           </Button>
         }
       />
