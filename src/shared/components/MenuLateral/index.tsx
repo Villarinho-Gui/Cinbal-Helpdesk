ed55d722fefbcbcc7ba5bd8ebacc1067e16b1ac4
Guilherme Villarinho <Guivillarinho15@gmail.com>
2023-04-16 12:47:28 -0300

    feat(src\shared\components\menulateral\index.tsx): :sparkles: Criação do Drawer

---
+++ src/shared/components/MenuLateral/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Drawer } from '@mui/material'
+
+// import { Container } from './styles';
+
+const MenuLateral: React.FC = () => {
+  return <Drawer>teste</Drawer>
+}
+
+export default MenuLateral
