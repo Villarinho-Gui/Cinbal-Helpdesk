096a9df879beb446316f480449d4a42fbcccafd4
Guilherme Villarinho <Guivillarinho15@gmail.com>
2023-04-16 12:49:07 -0300

    feat(app.tsx): :sparkles: Importação do Drwaer
    Drawer*

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from './routes'
 import { AppThemeProvider } from './shared/contexts'
+import MenuLateral from './shared/components/MenuLateral'
 
 export const App: React.FC = () => {
   return (
     <AppThemeProvider>
       <BrowserRouter>
+        <MenuLateral />
         <AppRoutes />
       </BrowserRouter>
     </AppThemeProvider>
