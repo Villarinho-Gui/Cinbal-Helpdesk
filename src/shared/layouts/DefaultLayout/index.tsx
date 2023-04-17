0708ed62f6ccecac8f384010aecc50d45f57c92f
Guilherme Villarinho <Guivillarinho15@gmail.com>
2023-04-17 19:35:30 -0300

    feat(src\shared\layouts\defaultlayout\index.tsx): :sparkles: Começo da Barra de Ferramentas
    Configurações iniciais da barra de ferramentas, além  de algumas aplicações para responsividade.

--- src/shared/layouts/DefaultLayout/index.tsx
+++ src/shared/layouts/DefaultLayout/index.tsx
@@ -5,31 +5,33 @@ import {
   Typography,
   useMediaQuery,
   useTheme,
-} from "@mui/material";
-import React from "react";
+} from '@mui/material'
+import React, { ReactNode } from 'react'
 
-import { GiHamburgerMenu } from "react-icons/gi";
-import { useDrawerContext } from "../../contexts/DrawerContext";
-
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { useDrawerContext } from '../../contexts/DrawerContext'
 interface IDefaultLayoutProps {
-  children: React.ReactNode;
-  tituloPagina: string;
+  children: React.ReactNode
+  tituloPagina: string
+  barraDeFerramentas?: ReactNode
 }
 
 const DefaultLayout: React.FC<IDefaultLayoutProps> = ({
   children,
   tituloPagina,
+  barraDeFerramentas,
 }) => {
-  const theme = useTheme();
-  const smDown = useMediaQuery(theme.breakpoints.down("sm"));
+  const theme = useTheme()
+  const smDown = useMediaQuery(theme.breakpoints.down('sm'))
+  const mdDown = useMediaQuery(theme.breakpoints.down('md'))
 
-  const {toggleDrawerOpen} = useDrawerContext();
+  const { toggleDrawerOpen } = useDrawerContext()
 
   return (
     <Box height="100%" display="flex" flexDirection="column" gap={1}>
       <Box
         padding={1}
-        height={theme.spacing(12)}
+        height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)}
         display="flex"
         alignItems="center"
         gap={2}
@@ -42,14 +44,22 @@ const DefaultLayout: React.FC<IDefaultLayoutProps> = ({
           </IconButton>
         )}
 
-        <Typography variant="h4" component="h4">
+        <Typography
+          variant={smDown ? 'h5' : mdDown ? 'h4' : 'h3'}
+          component="h4"
+          whiteSpace="nowrap"
+          overflow="hidden"
+          textOverflow="elipses"
+        >
           {tituloPagina}
         </Typography>
       </Box>
-      <Box>Barra de Ferramentas</Box>
-      <Box>{children}</Box>
+      {barraDeFerramentas && <Box>{barraDeFerramentas}</Box>}
+      <Box flex={1} overflow="auto">
+        {children}{' '}
+      </Box>
     </Box>
-  );
-};
+  )
+}
 
-export default DefaultLayout;
+export default DefaultLayout
