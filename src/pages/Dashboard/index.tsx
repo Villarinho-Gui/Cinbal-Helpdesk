6f66744d0dd3106be1989234951ff2f3b54bc34d
Guilherme Villarinho <Guivillarinho15@gmail.com>
2023-04-17 19:40:47 -0300

    feat(src\pages\dashboard\index.tsx): :sparkles: chamando barraDeFerramentas

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
-import React from "react";
-import DefaultLayout from "../../shared/layouts/DefaultLayout";
+/* eslint-disable prettier/prettier */
+import React from 'react'
+import DefaultLayout from '../../shared/layouts/DefaultLayout'
 
 const Dashboard: React.FC = () => {
-  return <DefaultLayout tituloPagina="Abrir Chamado">Testando</DefaultLayout>;
-};
+  return <DefaultLayout tituloPagina="Abrir Chamado" barraDeFerramentas={<>barra de ferramentas</>}>Testando</DefaultLayout>
+}
 
-export default Dashboard;
+export default Dashboard
