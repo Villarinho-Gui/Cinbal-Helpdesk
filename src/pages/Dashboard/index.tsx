0ab8a104755011b11e16251fe0d698de2ec329ff
guilhermevillarinho <Guivillarinho15@gmail.com>
2023-04-17 16:59:26 -0300

    feat(src\pages\dashboard\index.tsx): :sparkles: Teste de layout

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import DefaultLayout from "../../shared/layouts/DefaultLayout";
+
+const Dashboard: React.FC = () => {
+  return <DefaultLayout tituloPagina="Abrir Chamado">Testando</DefaultLayout>;
+};
+
+export default Dashboard;
