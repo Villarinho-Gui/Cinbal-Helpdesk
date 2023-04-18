af5a7e7062461e2176be5509487a269b5f63e697
guilhermevillarinho <Guivillarinho15@gmail.com>
2023-04-18 11:02:24 -0300

    feat(src\pages\dashboard): :sparkles: Importando componente Barra de Ferramentas

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable prettier/prettier */
 import React from 'react'
 import DefaultLayout from '../../shared/layouts/DefaultLayout'
+import BarraFerramentasAbrirChamado from '../../shared/components/BarraFerramentasAbrirChamado'
 
 const Dashboard: React.FC = () => {
-  return <DefaultLayout tituloPagina="Abrir Chamado" barraDeFerramentas={<>barra de ferramentas</>}>Testando</DefaultLayout>
+  return <DefaultLayout tituloPagina="Abrir Chamado" barraDeFerramentas={(<BarraFerramentasAbrirChamado mostrarInputBusca />)}>Testando</DefaultLayout>
 }
 
 export default Dashboard
