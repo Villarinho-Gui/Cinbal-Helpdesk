c6d1b58b3bef4d20fdfc99b78208252a1c81bb3c
guilhermevillarinho <Guivillarinho15@gmail.com>
2023-04-18 13:38:23 -0300

    feat(barraferramentasdetalheschamado): :sparkles: Configurações Iniciais
    Configurações da barra de ferramentas da criação do chamado

---
+++ src/shared/components/BarraFerramentasDetalhesChamado/index.tsx
@@ -0,0 +1,82 @@
+import { Box, Divider, Icon, IconButton, useTheme } from '@mui/material'
+import React from 'react'
+import { BiPlus } from 'react-icons/bi'
+import { AiOutlineArrowLeft } from 'react-icons/ai'
+import { MdDelete, MdSave } from 'react-icons/md'
+
+interface IBarraFerramentasDetalhesChamadoProps {
+  mostrarBotaoVoltar?: boolean
+  mostrarBotaoNovo?: boolean
+  mostrarBotaoSalvar?: boolean
+  mostrarBotaoDeletar?: boolean
+
+  aoClicarEmVoltar?: () => void
+  aoClicarEmNovo?: () => void
+  aoClicarEmSalvar?: () => void
+  aoClicarEmDeletar?: () => void
+}
+
+const BarraFerramentasDetalhesChamado: React.FC<
+  IBarraFerramentasDetalhesChamadoProps
+> = ({
+  mostrarBotaoVoltar = true,
+  mostrarBotaoNovo = true,
+  mostrarBotaoSalvar = true,
+  mostrarBotaoDeletar = true,
+
+  aoClicarEmVoltar,
+  aoClicarEmNovo,
+  aoClicarEmSalvar,
+  aoClicarEmDeletar,
+}) => {
+  const theme = useTheme()
+
+  return (
+    <Box
+      border="1px solid"
+      borderColor={theme.palette.divider}
+      borderRadius={1}
+      display="flex"
+      alignItems="center"
+      padding={1}
+      paddingX={1}
+      marginX={1}
+      gap={1}
+      height={theme.spacing(5)}
+    >
+      {mostrarBotaoVoltar && (
+        <IconButton onClick={aoClicarEmVoltar}>
+          <Icon>
+            <AiOutlineArrowLeft />
+          </Icon>
+        </IconButton>
+      )}
+      <Divider variant="middle" orientation="vertical" />
+      <Box display="flex" flex={1} justifyContent="end">
+        {mostrarBotaoNovo && (
+          <IconButton onClick={aoClicarEmNovo}>
+            <Icon>
+              <BiPlus />
+            </Icon>
+          </IconButton>
+        )}
+        {mostrarBotaoSalvar && (
+          <IconButton onClick={aoClicarEmSalvar}>
+            <Icon>
+              <MdSave />
+            </Icon>
+          </IconButton>
+        )}
+        {mostrarBotaoDeletar && (
+          <IconButton onClick={aoClicarEmDeletar}>
+            <Icon>
+              <MdDelete />
+            </Icon>
+          </IconButton>
+        )}
+      </Box>
+    </Box>
+  )
+}
+
+export default BarraFerramentasDetalhesChamado
