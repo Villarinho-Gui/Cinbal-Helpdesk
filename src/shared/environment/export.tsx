4cc4f2092386dbcc591311d87fbdba764bb7aba7
guilhermevillarinho <Guivillarinho15@gmail.com>
2023-04-18 15:55:34 -0300

    feat(src\shared\environment\export.tsx): :sparkles:Definição do Environment

---
+++ src/shared/environment/export.tsx
@@ -0,0 +1,18 @@
+export const Environment = {
+  /**
+   * Define a quantidade de chamados que irão ser carregados por padrão na listagem
+   */
+  LIMITE_DE_LINHAS: 10,
+  /**
+   *O que irá aparecer por padrão nos componentes de busca
+   */
+  INPUT_DE_BUSCA: 'Pesquisar...',
+  /**
+   *Quando não encontrar nenhum chamado
+   */
+  LISTAGEM_VAZIA: 'Nenhum chamado encontrado',
+  /**
+   *Link padrão do servidor para ser "escutado" pelo Axios
+   */
+  URL_BASE: 'http://localhost:3333',
+}
