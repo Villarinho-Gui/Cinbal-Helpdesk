b133a5cd17823ab29c32e353e26266b656703048
guilhermevillarinho <Guivillarinho15@gmail.com>
2023-04-19 07:57:12 -0300

    docs(src\shared\services\api\chamados): :open_file_folder: Alteração de nome
    De pessoas para Chamados

---
+++ src/shared/services/api/Chamados/ChamadosServices.tsx
@@ -0,0 +1,108 @@
+import { Environment } from '../../../environment/export'
+import { Api } from '../Config'
+
+interface IListagemChamados {
+  id: number
+  titulo: string
+  categoria: string
+  descricao: string
+}
+
+interface IDetalheChamados {
+  id: number
+  chamadoId: number
+  author: string
+  titulo: string
+  categoria: string
+  attachedFile: [0]
+  descricao: string
+  publishedAt: number
+}
+
+type IChamadoComTotalCount = {
+  data: IListagemChamados[]
+  totalCount: string
+}
+
+const getAll = async (
+  page = 1,
+  filter = '',
+): Promise<IChamadoComTotalCount | Error> => {
+  try {
+    const urlRelativa = `/chamado?_page=${page}&_limit=${Environment.LIMITE_DE_LINHAS}&titulo_like=${filter}`
+    const { data, headers } = await Api.get(urlRelativa)
+    if (data) {
+      return {
+        data,
+        totalCount: headers['x-total-count'],
+      }
+    }
+    return new Error('Erro ao listar os chamados')
+  } catch (error) {
+    console.error(error)
+    return new Error(
+      (error as { message: string }).message || 'Erro ao listar os chamados',
+    )
+  }
+}
+const getById = async (id: number): Promise<IDetalheChamados | Error> => {
+  try {
+    const { data } = await Api.get(`/chamado/${id}`)
+    if (data) {
+      return data
+    }
+    return new Error('Erro ao consultar o chamado')
+  } catch (error) {
+    console.error(error)
+    return new Error(
+      (error as { message: string }).message || 'Erro ao consultar o chamado',
+    )
+  }
+}
+const create = async (
+  dados: Omit<IDetalheChamados, 'id'>,
+): Promise<number | Error> => {
+  try {
+    const { data } = await Api.post<IDetalheChamados>('/chamado', dados)
+    if (data) {
+      return data.id
+    }
+    return new Error('Erro ao criar chamado')
+  } catch (error) {
+    console.error(error)
+    return new Error(
+      (error as { message: string }).message || 'Erro ao criar chamado',
+    )
+  }
+}
+const updateById = async (
+  id: number,
+  dados: IDetalheChamados,
+): Promise<void | Error> => {
+  try {
+    await Api.put(`/chamado/${id}`, dados)
+  } catch (error) {
+    console.error(error)
+    return new Error(
+      (error as { message: string }).message || 'Erro ao atualizar chamado',
+    )
+  }
+}
+const deleteById = async (id: number): Promise<void | Error> => {
+  try {
+    await Api.delete(`/chamado/${id}`)
+  } catch (error) {
+    console.error(error)
+    return new Error(
+      (error as { message: string }).message || 'Erro ao deletar chamado',
+    )
+  }
+}
+
+export const PessoasService = {
+  getAll,
+  getById,
+  create,
+  updateById,
+  deleteById,
+}
