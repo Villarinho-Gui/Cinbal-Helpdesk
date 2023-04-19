474ebabb30f047080029da3aa64bd0c99153942d
guilhermevillarinho <Guivillarinho15@gmail.com>
2023-04-19 16:46:18 -0300

    feat(src\routes): :sparkles: Rota AbrirChamado
    Criação da rota para abrir novos chamados

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import { Route, Routes, Navigate } from 'react-router-dom'
 import Dashboard from '../pages/Dashboard'
+import AbrirChamado from '../pages/AbrirChamado'
 
 export const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/home" element={<Dashboard />} />
+      <Route path="/abrir-chamado" element={<AbrirChamado />} />
       <Route path="*" element={<Navigate to="/home" />} />
     </Routes>
   )
