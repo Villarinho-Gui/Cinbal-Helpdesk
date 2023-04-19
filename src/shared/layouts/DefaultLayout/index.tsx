076dce28c9d5f24f5b0b5247bf05af6de80b18ee
guilhermevillarinho <Guivillarinho15@gmail.com>
2023-04-19 09:51:42 -0300

    feat(defaultlayout): :sparkles: Permitindo escolha de visualização do botão

--- src/shared/layouts/DefaultLayout/index.tsx
+++ src/shared/layouts/DefaultLayout/index.tsx
@@ -11,25 +11,31 @@ import React, { ReactNode } from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { useDrawerContext } from '../../contexts/DrawerContext'
 
-import { BsMoonFill } from "react-icons/bs"
+import { BsMoonFill } from 'react-icons/bs'
 import { useAppThemeContext } from '../../contexts/ThemeContext'
 interface IDefaultLayoutProps {
   children: React.ReactNode
   tituloPagina: string
   barraDeFerramentas?: ReactNode
+
+  mostrarBotaoTema?: boolean
+  mostrarTituloPagina?: boolean
 }
 
 const DefaultLayout: React.FC<IDefaultLayoutProps> = ({
   children,
   tituloPagina,
   barraDeFerramentas,
+  mostrarBotaoTema,
+
+  mostrarTituloPagina = true,
 }) => {
   const theme = useTheme()
   const smDown = useMediaQuery(theme.breakpoints.down('sm'))
   const mdDown = useMediaQuery(theme.breakpoints.down('md'))
 
   const { toggleDrawerOpen } = useDrawerContext()
-  const { toggleTheme } = useAppThemeContext();
+  const { toggleTheme } = useAppThemeContext()
 
   return (
     <Box height="100%" display="flex" flexDirection="column" gap={1}>
@@ -48,21 +54,36 @@ const DefaultLayout: React.FC<IDefaultLayoutProps> = ({
           </IconButton>
         )}
 
-        <Box display="flex" alignItems="center" justifyContent="space-between" width="100%">
-          <Typography
-            variant={smDown ? 'h5' : mdDown ? 'h4' : 'h3'}
-            component="h4"
-            whiteSpace="nowrap"
-            overflow="hidden"
-            textOverflow="elipses"
-          >
-            {tituloPagina}
-          </Typography>
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+          width="100%"
+        >
+          {mostrarTituloPagina && (
+            <Typography
+              variant={smDown ? 'h5' : mdDown ? 'h4' : 'h3'}
+              component="h4"
+              whiteSpace="nowrap"
+              overflow="hidden"
+              textOverflow="elipses"
+            >
+              {tituloPagina}
+            </Typography>
+          )}
 
-          <IconButton size='small' sx={{height: "35px", position: "relative", right: "20px"}} onClick={toggleTheme}><Icon><BsMoonFill /></Icon></IconButton>
+          {mostrarBotaoTema && (
+            <IconButton
+              size="small"
+              sx={{ height: '35px', position: 'relative', right: '20px' }}
+              onClick={toggleTheme}
+            >
+              <Icon>
+                <BsMoonFill />
+              </Icon>
+            </IconButton>
+          )}
         </Box>
-
-        
       </Box>
       {barraDeFerramentas && <Box>{barraDeFerramentas}</Box>}
       <Box flex={1} overflow="auto">
