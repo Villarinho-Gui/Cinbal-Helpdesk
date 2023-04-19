a53ead006bec1c8871e281d7846a30d8e0df4089
guilhermevillarinho <Guivillarinho15@gmail.com>
2023-04-19 11:00:04 -0300

    feat(components\listagemdechamados): :sparkles: Importando o useDebounce

--- src/shared/components/ListagemDeChamados/index.tsx
+++ src/shared/components/ListagemDeChamados/index.tsx
@@ -5,21 +5,27 @@ import { Chamado } from '../Chamado'
 import { useSearchParams } from 'react-router-dom'
 import { ChamadosService } from '../../services/api/Chamados/ChamadosServices'
 
+import { useDebounce } from '../../hooks/UseDebounce'
+
 // import { Container } from './styles';
 
 export const ListagemDeChamados: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams()
+
+  const { debounce } = useDebounce(3000, false)
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || ''
   }, [searchParams])
 
   useEffect(() => {
-    ChamadosService.getAll(1, busca).then((result) => {
-      if (result instanceof Error) {
-        alert(result.message)
-      }
-      console.log(result)
+    debounce(() => {
+      ChamadosService.getAll(1, busca).then((result) => {
+        if (result instanceof Error) {
+          alert(result.message)
+        }
+        console.log(result)
+      })
     })
   }, [busca])
 
