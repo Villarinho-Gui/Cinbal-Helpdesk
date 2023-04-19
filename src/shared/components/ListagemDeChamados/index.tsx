412859b22bbaea9a17705dad99a1e90f3fbc0ebc
guilhermevillarinho <Guivillarinho15@gmail.com>
2023-04-19 14:39:45 -0300

    feat(listagemdechamados): :sparkles: Funcionalidades novas da listagem
    Serão criadas novas funcionalidades futuramente

--- src/shared/components/ListagemDeChamados/index.tsx
+++ src/shared/components/ListagemDeChamados/index.tsx
@@ -1,30 +1,43 @@
-import React, { useEffect, useMemo } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import DefaultLayout from '../../layouts/DefaultLayout'
 import BarraFerramentasAbrirChamado from '../BarraFerramentasAbrirChamado'
 import { Chamado } from '../Chamado'
 import { useSearchParams } from 'react-router-dom'
-import { ChamadosService } from '../../services/api/Chamados/ChamadosServices'
+import {
+  ChamadosService,
+  IListagemChamados,
+} from '../../services/api/Chamados/ChamadosServices'
 
 import { useDebounce } from '../../hooks/UseDebounce'
+import { List, ListItem } from '@mui/material'
 
 // import { Container } from './styles';
 
 export const ListagemDeChamados: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams()
 
-  const { debounce } = useDebounce(3000, false)
+  const { debounce } = useDebounce()
+
+  const [chamados, setChamados] = useState<IListagemChamados[]>([])
+  const [totalCount, setTotalCount] = useState<number | string>(0)
+  const [isLoading, setIsLoading] = useState<boolean>(true)
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || ''
   }, [searchParams])
 
   useEffect(() => {
+    setIsLoading(true)
     debounce(() => {
       ChamadosService.getAll(1, busca).then((result) => {
+        setIsLoading(false)
         if (result instanceof Error) {
           alert(result.message)
+        } else {
+          console.log(result)
+          setChamados(result.data)
+          setTotalCount(result.totalCount)
         }
-        console.log(result)
       })
     })
   }, [busca])
@@ -45,7 +58,18 @@ export const ListagemDeChamados: React.FC = () => {
         />
       }
     >
-      <Chamado />
+      <List sx={{ overflow: 'auto' }}>
+        {chamados.map((chamado) => (
+          <ListItem key={chamado.id}>
+            <Chamado
+              titulo={chamado.titulo}
+              categoria={chamado.categoria}
+              descricao={chamado.descricao}
+              id={chamado.id}
+            />
+          </ListItem>
+        ))}
+      </List>
     </DefaultLayout>
   )
 }
