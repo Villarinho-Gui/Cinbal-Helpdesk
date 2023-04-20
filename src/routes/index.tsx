1fdb0ee6849394d3c2287acc0f7749f2e73cb783
guilhermevillarinho <Guivillarinho15@gmail.com>
2023-04-20 15:50:50 -0300

    feat(src\routes): :sparkles: Adicionando rota de detalhe do chamado

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 import { Route, Routes, Navigate } from 'react-router-dom'
 import Dashboard from '../pages/Dashboard'
 import AbrirChamado from '../pages/AbrirChamado'
+import ChamadoDetalhe from '../pages/ChamadoDetalhe'
 
 export const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/home" element={<Dashboard />} />
       <Route path="/abrir-chamado" element={<AbrirChamado />} />
+      <Route path="/chamado/detalhe/:id" element={<ChamadoDetalhe />} />
       <Route path="*" element={<Navigate to="/home" />} />
     </Routes>
   )
