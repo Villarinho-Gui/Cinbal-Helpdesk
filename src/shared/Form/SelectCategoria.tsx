f1daba1c646ad31cc46abccacfb2c301d1a9cbd4
guilhermevillarinho <Guivillarinho15@gmail.com>
2023-04-25 17:11:01 -0300

    refactor(shared\form\selectcategoria): :dizzy: Novo nome do arquivo

---
+++ src/shared/Form/SelectCategoria.tsx
@@ -0,0 +1,64 @@
+import { MenuItem, Select, SelectProps } from '@mui/material'
+import { useField } from '@unform/core'
+import React, { useEffect, useState } from 'react'
+
+type TVSelectValue = SelectProps & {
+  name: string
+}
+
+interface ICategoria {
+  categoria:
+    | 'Email'
+    | 'Telefone'
+    | 'Rede'
+    | 'Fluig'
+    | 'Hardware'
+    | 'Software'
+    | 'PCFactory'
+    | 'Preactor'
+    | 'Protheus'
+    | 'Vexon '
+    | 'PortalDoCliente'
+    | 'Outros'
+}
+
+export const SelectCategoria: React.FC<TVSelectValue> = ({ name, ...rest }) => {
+  const { fieldName, registerField, defaultValue, error } = useField(name)
+
+  const [categoria, setCategoria] = useState<ICategoria>(defaultValue || '')
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      getValue: () => categoria,
+      setValue: (_, newValue) => setCategoria(newValue),
+    })
+  }, [registerField, fieldName, categoria])
+
+  return (
+    <Select
+      {...rest}
+      value={categoria}
+      label="Categoria"
+      error={!!error}
+      // helperText={error}
+      size="medium"
+      sx={{ width: '350px' }}
+      onChange={(e) => {
+        setCategoria(e.target.value)
+      }}
+    >
+      <MenuItem value="Email">Email</MenuItem>
+      <MenuItem value="Telefone">Telefone</MenuItem>
+      <MenuItem value="Fluig">Fluig</MenuItem>
+      <MenuItem value="Hardware">Hardware</MenuItem>
+      <MenuItem value="Software">Software</MenuItem>
+      <MenuItem value="PCFactory">PC Factory</MenuItem>
+      <MenuItem value="Preactor">Preactor</MenuItem>
+      <MenuItem value="Protheus">Protheus</MenuItem>
+      <MenuItem value="Vexon">Vexon</MenuItem>
+      <MenuItem value="PortalDoCliente">Portal do Cliente</MenuItem>
+      <MenuItem value="Outros">Outros</MenuItem>
+    </Select>
+  )
+}
