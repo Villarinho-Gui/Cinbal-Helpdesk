7556feb75c32c70664d557127db4196599c9dd21
guilhermevillarinho <Guivillarinho15@gmail.com>
2023-04-25 17:07:15 -0300

    feat(pages\abrirchamado): :sparkles: Importando o AutoComplete

--- src/pages/AbrirChamado/index.tsx
+++ src/pages/AbrirChamado/index.tsx
@@ -9,6 +9,7 @@ import { ChamadosService } from '../../shared/services/api/Chamados/ChamadosServ
 import { AiOutlinePaperClip } from 'react-icons/ai'
 
 import * as yup from 'yup'
+import { AutoCompleteCategoria } from '../../shared/Form/AutoCompleteCategoria'
 
 interface IFormData {
   author: string
@@ -128,12 +129,7 @@ const AbrirChamado: React.FC = () => {
             </Grid>
             <Grid container item direction="row" spacing={2}>
               <Grid item lg={4} sm={12} xs={12}>
-                <VTextField
-                  type="text"
-                  name="categoria"
-                  placeholder="Categoria"
-                  fullWidth
-                />
+                <AutoCompleteCategoria name="categoria" />
               </Grid>
               <Grid item lg={4} sm={12} xs={12} sx={{ marginY: 'auto' }}>
                 <Tooltip title="Anexar arquivo" placement="top" arrow>
