683e29ad93749bf8d1168a8b2382ad09542f69ee
Guilherme Villarinho <Guivillarinho15@gmail.com>
2023-04-26 20:16:22 -0300

    feat(shared\contexts\authcontext.tsx): :sparkles: Criação de contexto
    Contexto para realização de login e logout, com local storage e token de acesso.

--- src/shared/contexts/AuthContext.tsx
+++ src/shared/contexts/AuthContext.tsx
@@ -1,4 +1,12 @@
-import React, { Children, createContext } from 'react'
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, {
+  createContext,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react'
+import { AuthService } from '../services/api/auth/AuthService'
 
 interface IAuthContextData {
   isAuthenticated: boolean
@@ -12,11 +20,46 @@ interface IChildrenConfig {
 
 const AuthContext = createContext({} as IAuthContextData)
 
+const LOCAL_STORAGE_KEY__ACCESS_TOKEN = 'APP_ACCESS_TOKEN'
+
 const contexts: React.FC<IChildrenConfig> = ({ children }) => {
-  const trigge
+  const [accessToken, setAccessToken] = useState<string>()
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem(LOCAL_STORAGE_KEY__ACCESS_TOKEN)
+
+    if (accessToken) {
+      setAccessToken(JSON.parse(accessToken))
+    } else {
+      setAccessToken(undefined)
+    }
+  }, [])
+
+  const triggerLogin = useCallback(async (email: string, password: string) => {
+    const result = await AuthService.auth(email, password)
+
+    if (result instanceof Error) {
+      return result.message
+    } else {
+      localStorage.setItem(
+        LOCAL_STORAGE_KEY__ACCESS_TOKEN,
+        JSON.stringify(result.accessToken),
+      )
+      setAccessToken(result.accessToken)
+    }
+  }, [])
+  const triggerLogout = useCallback(() => {
+    localStorage.removeItem(LOCAL_STORAGE_KEY__ACCESS_TOKEN)
+
+    setAccessToken(undefined)
+  }, [])
+
+  const isAuthenticated = useMemo(() => !!accessToken, [accessToken])
 
   return (
-    <AuthContext.Provider value={(isAuthenticated, login, logout)}>
+    <AuthContext.Provider
+      value={{ isAuthenticated, login: triggerLogin, logout: triggerLogout }}
+    >
       {children}
     </AuthContext.Provider>
   )
