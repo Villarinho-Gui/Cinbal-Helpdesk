06e4023f49d2f9db8198844c40b6021e182e7e2e
Guilherme Villarinho <guivillarinho15@gmail.com>
2023-05-06 19:32:47 -0300

    refactor: :dizzy: Comentando desnecessidades temporárias

--- src/pages/AbrirChamado/index.tsx
+++ src/pages/AbrirChamado/index.tsx
@@ -5,7 +5,7 @@ import { Box, Grid, useTheme } from '@mui/material'
 
 import { useNavigate, useParams } from 'react-router-dom'
 import { VTextField, VForm, useVForm } from '../../shared/Form/export'
-import { ChamadosService } from '../../shared/services/api/Chamados/ChamadosServices'
+// import { ChamadosService } from '../../shared/services/api/Chamados/ChamadosServices'
 
 import * as yup from 'yup'
 import { SelectCategoria } from '../../shared/Form/SelectCategoria'
@@ -44,58 +44,58 @@ const AbrirChamado: React.FC = () => {
 
   const { formRef, save, isSaveAndClose, reset } = useVForm()
 
-  const triggerSave = (dados: IFormData) => {
-    formValidationSchema
-      .validate(dados, { abortEarly: false })
-      .then((dadosValidados) => {
-        if (id === 'novo') {
-          setIsLoading(true)
-          ChamadosService.create(dadosValidados).then((result) => {
-            setIsLoading(true)
-            if (result instanceof Error) {
-              alert(result.message)
-            } else {
-              if (isSaveAndClose()) {
-                navigate(`/chamados`)
-              } else {
-                navigate(`/chamados/detalhe/${result}`)
-              }
-            }
-          })
-        } else {
-          ChamadosService.updateById(Number(id), {
-            id: Number(id),
-            ...dadosValidados,
-            chamadoId: 0,
-            setor: '',
-            categoria: '',
-            attachedFile: [0],
-            author: '',
-            publishedAt: Date,
-          }).then((result) => {
-            setIsLoading(true)
-            if (result instanceof Error) {
-              alert(result.message)
-            } else {
-              navigate(`/chamados/detalhe/${result}/${id}`)
-            }
-          })
-        }
-      })
-      .catch((errors: yup.ValidationError) => {
-        const validationErrors: { [key: string]: string } = {}
+  // const triggerSave = (dados: IFormData) => {
+  //   formValidationSchema
+  //     .validate(dados, { abortEarly: false })
+  //     .then((dadosValidados) => {
+  //       if (id === 'novo') {
+  //         setIsLoading(true)
+  //         ChamadosService.create(dadosValidados).then((result) => {
+  //           setIsLoading(true)
+  //           if (result instanceof Error) {
+  //             alert(result.message)
+  //           } else {
+  //             if (isSaveAndClose()) {
+  //               navigate(`/chamados`)
+  //             } else {
+  //               navigate(`/chamados/detalhe/${result}`)
+  //             }
+  //           }
+  //         })
+  //       } else {
+  //         ChamadosService.updateById(Number(id), {
+  //           id: Number(id),
+  //           ...dadosValidados,
+  //           chamadoId: 0,
+  //           setor: '',
+  //           categoria: '',
+  //           attachedFile: [0],
+  //           author: '',
+  //           publishedAt: Date,
+  //         }).then((result) => {
+  //           setIsLoading(true)
+  //           if (result instanceof Error) {
+  //             alert(result.message)
+  //           } else {
+  //             navigate(`/chamados/detalhe/${result}/${id}`)
+  //           }
+  //         })
+  //       }
+  //     })
+  //     .catch((errors: yup.ValidationError) => {
+  //       const validationErrors: { [key: string]: string } = {}
 
-        errors.inner.forEach((error) => {
-          // eslint-disable-next-line no-useless-return
-          if (!error.path) return
+  //       errors.inner.forEach((error) => {
+  //         // eslint-disable-next-line no-useless-return
+  //         if (!error.path) return
 
-          validationErrors[error.path] = error.message
-        })
-        console.log(validationErrors)
+  //         validationErrors[error.path] = error.message
+  //       })
+  //       console.log(validationErrors)
 
-        formRef.current?.setErrors(validationErrors)
-      })
-  }
+  //       formRef.current?.setErrors(validationErrors)
+  //     })
+  // }
 
   return (
     <DefaultLayout
@@ -119,7 +119,7 @@ const AbrirChamado: React.FC = () => {
         border="1px solid"
         borderColor={theme.palette.divider}
       >
-        <VForm ref={formRef} onSubmit={triggerSave} action="POST">
+        {/* <VForm ref={formRef} onSubmit={triggerSave} action="POST">
           <Grid container direction="column" padding={2} spacing={2}>
             <Grid container item direction="row">
               <Grid item lg={6} sm={12} xs={12}>
@@ -155,7 +155,7 @@ const AbrirChamado: React.FC = () => {
           <VTextField name="author" placeholder="Autor" fullWidth />
 
           <VTextField name="publishedAt" placeholder="publishedAt" fullWidth />
-        </VForm>
+        </VForm> */}
       </Box>
     </DefaultLayout>
   )
