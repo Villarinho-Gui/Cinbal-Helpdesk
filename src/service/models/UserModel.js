72bbf3c86ab8ca50efa6abff4660df4874a0685b
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-05-08 10:56:58 -0300

    feat(service\models\usermodel.js): :sparkles: Model de usuário
    Ainda haverá novos campos.

---
+++ src/service/models/UserModel.js
@@ -0,0 +1,20 @@
+import { Sequelize } from 'sequelize'
+import db from './db'
+
+export default db.define('user', {
+  id: {
+    type: Sequelize.INTEGER.UNSIGNED,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  nome: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+  },
+})
