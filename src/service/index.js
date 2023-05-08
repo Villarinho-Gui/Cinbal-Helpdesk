0c3444773bc7d0d001c7305efcd63b4c7073d0c3
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-05-08 16:55:33 -0300

    refactor(service\index.js): :dizzy: Simplificando

--- src/service/index.js
+++ src/service/index.js
@@ -3,6 +3,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import UserModel from './models/UserModel.js'
+import bcrypt from 'bcryptjs'
 
 const app = express()
 
@@ -21,12 +22,14 @@ app.use((_, res, next) => {
 })
 
 app.post('/login/cadastro', async (req, res) => {
-  const nome = req.body.nome
-  const email = req.body.email
-  const ramal = req.body.ramal
-  const funcao = req.body.funcao
-  const setor = req.body.setor
-  const password = req.body.password
+  const data = req.body
+
+  const nome = data.nome
+  const email = data.email
+  const ramal = data.ramal
+  const funcao = data.funcao
+  const setor = data.setor
+  const password = await bcrypt.hash(data.password, 8)
 
   UserModel.create({
     nome,
