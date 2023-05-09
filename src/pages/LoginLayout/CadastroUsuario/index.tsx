e188394e79477ad027a680768045a29dc3e80e83
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-05-09 13:40:05 -0300

    refactor(pages\loginlayout\cadastrousuario\index.tsx): :dizzy: Atualizando formulário
    Adicionando responsividade e removendo desnecessidades.

--- src/pages/LoginLayout/CadastroUsuario/index.tsx
+++ src/pages/LoginLayout/CadastroUsuario/index.tsx
@@ -4,15 +4,14 @@ import { useForm, SubmitHandler } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
 import api from '../../../service/api/config/configApi'
 
-import DefaultLayout from '../../../shared/layouts/DefaultLayout'
 import {
-  Box,
-  useTheme,
-  Grid,
   Button,
   Typography,
   TextField,
   CircularProgress,
+  Grid,
+  Divider,
+  useTheme,
 } from '@mui/material'
 
 import * as yup from 'yup'
@@ -31,23 +30,12 @@ interface ICadastroUsuario {
 const createUserFormSchema = yup
   .object()
   .shape({
-    nome: yup
-      .string()
-      .required('Esse campo precisa ser preenchido!')
-      .min(3, 'Deve ter no mínimo 3 caracteres')
-      .max(50, 'Deve conter no máximo 50 caracteres'),
-    email: yup
-      .string()
-      .email('Formato de e-mail incorreto')
-      .required('Esse campo precisa ser preenchido!'),
-    ramal: yup
-      .string()
-      .required('Esse campo precisa ser preenchido!')
-      .min(2, 'Deve ter no mínimo 2 caracteres')
-      .max(5, 'Deve conter no máximo 5 caracteres'),
-    funcao: yup.string().required('Esse campo precisa ser preenchido!'),
-    setor: yup.string().required('Esse campo precisa ser preenchido!'),
-    filial: yup.string().required('Esse campo precisa ser preenchido!'),
+    nome: yup.string().required().min(3).max(50),
+    email: yup.string().email().required(),
+    ramal: yup.string().required().min(2).max(5),
+    funcao: yup.string().required(),
+    setor: yup.string().required(),
+    filial: yup.string().required(),
     password: yup
       .string()
       .required('Esse campo precisa ser preenchido!')
@@ -58,10 +46,8 @@ const createUserFormSchema = yup
 
 export const CadastroUsuario: React.FC = () => {
   const [isLoading, setIsLoading] = useState(false)
-  const [nome, setNome] = useState('')
-
-  const theme = useTheme()
   const navigate = useNavigate()
+  const theme = useTheme()
 
   const {
     register,
@@ -111,159 +97,189 @@ export const CadastroUsuario: React.FC = () => {
   }
 
   return (
-    <DefaultLayout tituloPagina={''}>
-      <Box
-        display="flex"
-        width="70%"
-        margin="auto"
-        borderRadius={1}
-        border="1px solid"
-        borderColor={theme.palette.divider}
+    <form id="form-cadastro" onSubmit={handleSubmit(createUser)} method="POST">
+      <Grid
+        container
+        direction="column"
+        padding={20}
+        spacing={2}
+        sx={{
+          [theme.breakpoints.down('sm')]: {
+            padding: '30px',
+          },
+        }}
       >
-        <form
-          onSubmit={handleSubmit(createUser)}
-          style={{ width: '100%' }}
-          method="post"
-        >
-          <Grid container direction="column" padding={5} spacing={2}>
-            <Grid container item lg={6} spacing={2}>
-              <Grid item lg={12} sm={12} xs={12}>
-                <Typography variant="h5" sx={{ marginBottom: '20px' }}>
-                  Cadastro de usuário
+        <Grid container item lg={6} spacing={2}>
+          <Grid item lg={12} sm={12} xs={12}>
+            <Typography
+              variant="h5"
+              sx={{ marginBottom: '5px', fontWeight: '400', fontSize: '16px' }}
+            >
+              Dados do colaborador
+            </Typography>
+            <Divider sx={{ marginBottom: '10px' }} />
+            <TextField
+              {...register('nome')}
+              name="nome"
+              error={!!errors.nome}
+              helperText={
+                <Typography variant="body2" color="error">
+                  {errors.nome && <span>{errors.nome?.message}</span>}
                 </Typography>
-                <TextField
-                  {...register('nome')}
-                  name="nome"
-                  error={!!errors.nome}
-                  helperText={
-                    <Typography variant="body2" color="error">
-                      {errors.nome && <span>{errors.nome?.message}</span>}
-                    </Typography>
-                  }
-                  type="text"
-                  placeholder="Nome completo"
-                  fullWidth
-                  onChange={(event) => setNome(event.target.value)}
-                  value={nome}
-                />
-              </Grid>
-              <Grid item lg={12} sm={12} xs={12}>
-                <TextField
-                  {...register('email')}
-                  name="email"
-                  error={!!errors.email}
-                  helperText={
-                    <Typography variant="body2" color="error">
-                      {errors.email && <span>{errors.email?.message}</span>}
-                    </Typography>
-                  }
-                  type="email"
-                  placeholder="E-mail"
-                  fullWidth
-                />
-              </Grid>
-              <Grid item lg={12} sm={12} xs={12}>
-                <TextField
-                  {...register('ramal')}
-                  name="ramal"
-                  error={!!errors.ramal}
-                  helperText={
-                    <Typography variant="body2" color="error">
-                      {errors.ramal && <span>{errors.ramal?.message}</span>}
-                    </Typography>
-                  }
-                  type="text"
-                  placeholder="Ramal"
-                  fullWidth
-                />
-              </Grid>
-              <Grid item lg={12} sm={12} xs={12}>
-                <TextField
-                  {...register('funcao')}
-                  name="funcao"
-                  error={!!errors.funcao}
-                  helperText={
-                    <Typography variant="body2" color="error">
-                      {errors.funcao && <span>{errors.funcao?.message}</span>}
-                    </Typography>
-                  }
-                  type="text"
-                  placeholder="Função"
-                  fullWidth
-                />
-              </Grid>
-              <Grid item lg={12} sm={12} xs={12}>
-                <TextField
-                  {...register('setor')}
-                  name="setor"
-                  error={!!errors.setor}
-                  helperText={
-                    <Typography variant="body2" color="error">
-                      {errors.setor && <span>{errors.setor?.message}</span>}
-                    </Typography>
-                  }
-                  type="text"
-                  placeholder="Setor"
-                  fullWidth
-                />
-              </Grid>
-              <Grid item lg={12} sm={12} xs={12}>
-                <TextField
-                  {...register('filial')}
-                  name="filial"
-                  error={!!errors.filial}
-                  helperText={
-                    <Typography variant="body2" color="error">
-                      {errors.filial && <span>{errors.filial?.message}</span>}
-                    </Typography>
-                  }
-                  type="text"
-                  placeholder="Filial"
-                  fullWidth
-                />
-              </Grid>
-              <Grid container item lg={12} sm={12} xs={12}>
-                <TextField
-                  {...register('password')}
-                  name="password"
-                  error={!!errors.password}
-                  helperText={
-                    <Typography variant="body2" color="error">
-                      {errors.password && (
-                        <span>{errors.password?.message}</span>
-                      )}
-                    </Typography>
-                  }
-                  type="password"
-                  placeholder="Senha"
-                  fullWidth
-                />
-                <Button
-                  type="submit"
-                  variant="contained"
-                  disableElevation
-                  sx={{ width: '100%', marginTop: '20px' }}
-                  endIcon={
-                    isLoading && <CircularProgress variant="indeterminate" />
-                  }
-                >
-                  cadastrar
-                </Button>
-                <Button
-                  type="submit"
-                  variant="outlined"
-                  sx={{ width: '100%', marginTop: '10px' }}
-                  onClick={() => navigate('/login')}
-                >
-                  Voltar
-                </Button>
-              </Grid>
+              }
+              type="text"
+              placeholder="Nome completo"
+              autoComplete="username"
+              fullWidth
+              size="small"
+            />
+          </Grid>
+          <Grid item lg={12} sm={12} xs={12}>
+            <TextField
+              {...register('email')}
+              name="email"
+              error={!!errors.email}
+              helperText={
+                <Typography variant="body2" color="error">
+                  {errors.email && <span>{errors.email?.message}</span>}
+                </Typography>
+              }
+              type="email"
+              placeholder="E-mail"
+              fullWidth
+              size="small"
+            />
+          </Grid>
+          <Grid item lg={12} sm={12} xs={12}>
+            <TextField
+              {...register('password')}
+              name="password"
+              error={!!errors.password}
+              helperText={
+                <Typography variant="body2" color="error">
+                  {errors.password && <span>{errors.password?.message}</span>}
+                </Typography>
+              }
+              type="password"
+              placeholder="Senha"
+              autoComplete="current-password"
+              fullWidth
+              size="small"
+            />
+          </Grid>
+          <Grid item lg={12} sm={12} xs={12}>
+            <Typography
+              variant="h5"
+              sx={{ marginBottom: '5px', fontWeight: '400', fontSize: '16px' }}
+            >
+              Informações profissionais da Cinbal
+            </Typography>
+            <Divider sx={{ marginBottom: '10px' }} />
+            <TextField
+              {...register('funcao')}
+              name="funcao"
+              error={!!errors.funcao}
+              helperText={
+                <Typography variant="body2" color="error">
+                  {errors.funcao && <span>{errors.funcao?.message}</span>}
+                </Typography>
+              }
+              type="text"
+              placeholder="Função"
+              fullWidth
+              size="small"
+            />
+          </Grid>
+          <Grid item lg={12} sm={12} xs={12}>
+            <TextField
+              {...register('setor')}
+              name="setor"
+              error={!!errors.setor}
+              helperText={
+                <Typography variant="body2" color="error">
+                  {errors.setor && <span>{errors.setor?.message}</span>}
+                </Typography>
+              }
+              type="text"
+              placeholder="Setor"
+              fullWidth
+              size="small"
+            />
+          </Grid>
+          <Grid
+            item
+            container
+            direction={'row'}
+            lg={12}
+            sm={12}
+            xs={12}
+            spacing={2}
+          >
+            <Grid item lg={6} xs={12}>
+              <TextField
+                {...register('ramal')}
+                name="ramal"
+                error={!!errors.ramal}
+                helperText={
+                  <Typography variant="body2" color="error">
+                    {errors.ramal && <span>{errors.ramal?.message}</span>}
+                  </Typography>
+                }
+                type="tel"
+                placeholder="Ramal"
+                fullWidth
+                inputMode="numeric"
+                size="small"
+              />
+            </Grid>
+            <Grid item lg={6} xs={12}>
+              <TextField
+                {...register('filial')}
+                name="filial"
+                error={!!errors.filial}
+                helperText={
+                  <Typography variant="body2" color="error">
+                    {errors.filial && <span>{errors.filial?.message}</span>}
+                  </Typography>
+                }
+                type="text"
+                placeholder="Filial"
+                fullWidth
+                size="small"
+              />
             </Grid>
           </Grid>
-
-          {}
-        </form>
-      </Box>
-    </DefaultLayout>
+          <Grid container item lg={6} sm={12} xs={12} spacing={2}>
+            <Grid item lg={6} md={12} sm={12} xs={12}>
+              <Button
+                type="submit"
+                variant="contained"
+                disableElevation
+                sx={{
+                  width: '100%',
+                  marginBottom: '10px',
+                  [theme.breakpoints.down('sm')]: { marginBottom: '0px' },
+                }}
+                endIcon={
+                  isLoading && <CircularProgress variant="indeterminate" />
+                }
+              >
+                cadastrar
+              </Button>
+            </Grid>
+            <Grid item lg={6} md={12} sm={12} xs={12}>
+              <Button
+                type="submit"
+                variant="outlined"
+                sx={{ width: '100%' }}
+                onClick={() => navigate('/login')}
+              >
+                Voltar
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </form>
   )
 }
