27fa29d9998b3bc724124798066252dab20124e9
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-05-10 17:02:26 -0300

    feat(pages\abrirchamado\index.tsx): :sparkles: Adicionando o novo formulário
    Ainda terá modificação no layout, na método de post para o banco e algumas outras funcionalidades

--- src/pages/AbrirChamado/index.tsx
+++ src/pages/AbrirChamado/index.tsx
@@ -1,21 +1,31 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
 import DefaultLayout from '../../shared/layouts/DefaultLayout'
-import { Box, Grid, useTheme } from '@mui/material'
+import {
+  Alert,
+  Box,
+  Button,
+  CircularProgress,
+  Divider,
+  Grid,
+  Snackbar,
+  TextField,
+  Typography,
+  useTheme,
+} from '@mui/material'
 
 import { useNavigate, useParams } from 'react-router-dom'
-import { VTextField, VForm, useVForm } from '../../shared/Form/export'
-// import { ChamadosService } from '../../shared/services/api/Chamados/ChamadosServices'
 
 import * as yup from 'yup'
-import { SelectCategoria } from '../../shared/Form/SelectCategoria'
+import { yupResolver } from '@hookform/resolvers/yup'
 import { BarraFerramentasAbrirNovoChamado } from '../../shared/components/BarraFerramentasAbrirNovoChamado'
+import { useForm } from 'react-hook-form'
+import api from '../../service/api/config/configApi'
 
 interface IFormData {
-  author: string
   titulo: string
+  categoria: string
   descricao: string
-  publishedAt: Date
 }
 
 const formValidationSchema = yup
@@ -42,60 +52,41 @@ const AbrirChamado: React.FC = () => {
   const theme = useTheme()
   const navigate = useNavigate()
 
-  const { formRef, save, isSaveAndClose, reset } = useVForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormData>({
+    resolver: yupResolver(formValidationSchema),
+    defaultValues: {
+      titulo: '',
+      categoria: '',
+      descricao: '',
+    },
+  })
 
-  // const triggerSave = (dados: IFormData) => {
-  //   formValidationSchema
-  //     .validate(dados, { abortEarly: false })
-  //     .then((dadosValidados) => {
-  //       if (id === 'novo') {
-  //         setIsLoading(true)
-  //         ChamadosService.create(dadosValidados).then((result) => {
-  //           setIsLoading(true)
-  //           if (result instanceof Error) {
-  //             alert(result.message)
-  //           } else {
-  //             if (isSaveAndClose()) {
-  //               navigate(`/chamados`)
-  //             } else {
-  //               navigate(`/chamados/detalhe/${result}`)
-  //             }
-  //           }
-  //         })
-  //       } else {
-  //         ChamadosService.updateById(Number(id), {
-  //           id: Number(id),
-  //           ...dadosValidados,
-  //           chamadoId: 0,
-  //           setor: '',
-  //           categoria: '',
-  //           attachedFile: [0],
-  //           author: '',
-  //           publishedAt: Date,
-  //         }).then((result) => {
-  //           setIsLoading(true)
-  //           if (result instanceof Error) {
-  //             alert(result.message)
-  //           } else {
-  //             navigate(`/chamados/detalhe/${result}/${id}`)
-  //           }
-  //         })
-  //       }
-  //     })
-  //     .catch((errors: yup.ValidationError) => {
-  //       const validationErrors: { [key: string]: string } = {}
+  const createUser = async (data: IFormData) => {
+    setIsLoading(false)
+    const formData = new FormData()
 
-  //       errors.inner.forEach((error) => {
-  //         // eslint-disable-next-line no-useless-return
-  //         if (!error.path) return
+    formData.append('titulo', data.titulo)
+    formData.append('categoria', data.categoria)
+    formData.append('descricao', data.descricao)
 
-  //         validationErrors[error.path] = error.message
-  //       })
-  //       console.log(validationErrors)
-
-  //       formRef.current?.setErrors(validationErrors)
-  //     })
-  // }
+    const headers = {
+      headers: {
+        'content-type': 'application/json',
+      },
+    }
+    try {
+      await api.post<IFormData>('/', formData, headers).then(() => {
+        navigate('/login')
+      })
+    } catch (error) {
+      console.log(error)
+    }
+    setIsLoading(true)
+  }
 
   return (
     <DefaultLayout
@@ -105,8 +96,6 @@ const AbrirChamado: React.FC = () => {
       mostrarBotaoPerfil
       barraDeFerramentas={
         <BarraFerramentasAbrirNovoChamado
-          aoCLicarEmEnviar={save}
-          aoClicarEmLimpar={reset}
           mostrarBotaoAnexarArquivo
           aoClicarEmAnexar={() => {}}
         />
@@ -119,43 +108,126 @@ const AbrirChamado: React.FC = () => {
         border="1px solid"
         borderColor={theme.palette.divider}
       >
-        {/* <VForm ref={formRef} onSubmit={triggerSave} action="POST">
-          <Grid container direction="column" padding={2} spacing={2}>
-            <Grid container item direction="row">
-              <Grid item lg={6} sm={12} xs={12}>
-                <VTextField
+        <form
+          id="form-cadastro"
+          onSubmit={handleSubmit(createUser)}
+          method="POST"
+        >
+          <Grid
+            container
+            direction="column"
+            padding={10}
+            spacing={2}
+            sx={{
+              [theme.breakpoints.down('sm')]: {
+                padding: '30px',
+              },
+            }}
+          >
+            <Grid container item lg={6} spacing={2}>
+              <Grid item lg={12} sm={12} xs={12} xl={6}>
+                <TextField
+                  {...register('titulo')}
+                  name="titulo"
                   type="text"
                   placeholder="Título"
-                  label="Título"
+                  // value={titulo}
+                  // onChange={(e) => setNomeCompleto(e.target.value)}
+                  error={!!errors.titulo}
+                  helperText={
+                    <Typography variant="body2" color="error">
+                      {errors.titulo && <span>{errors.titulo?.message}</span>}
+                    </Typography>
+                  }
+                  autoComplete="off"
                   fullWidth
-                  name="titulo"
+                  size="medium"
                 />
               </Grid>
-            </Grid>
-            <Grid container item direction="row" spacing={2}>
-              <Grid item lg={4} sm={12} xs={12}>
-                <SelectCategoria name="categoria" />
-              </Grid>
-            </Grid>
-            <Grid container item direction="row">
-              <Grid item lg={6} sm={8} xs={12}>
-                <VTextField
+              <Grid item lg={12} sm={12} xs={12} xl={4}>
+                <TextField
+                  {...register('categoria')}
+                  name="categoria"
+                  // value={email}
+                  // onChange={(e) => setEmail(e.target.value)}
+                  error={!!errors.categoria}
+                  helperText={
+                    <Typography variant="body2" color="error">
+                      {errors.categoria && (
+                        <span>{errors.categoria?.message}</span>
+                      )}
+                    </Typography>
+                  }
                   type="text"
-                  name="descricao"
-                  placeholder="Descrição"
-                  label="Descrição"
+                  placeholder="Categoria"
+                  size="medium"
                   fullWidth
-                  multiline
-                  rows={4}
                 />
+              </Grid>
+              <Grid item lg={12} sm={12} xs={12} xl={10}>
+                <TextField
+                  {...register('descricao')}
+                  name="descricao"
+                  error={!!errors.descricao}
+                  // value={password}
+                  // onChange={(e) => setPassword(e.target.value)}
+                  helperText={
+                    <Typography variant="body2" color="error">
+                      {errors.descricao && (
+                        <span>{errors.descricao?.message}</span>
+                      )}
+                    </Typography>
+                  }
+                  multiline
+                  rows={5}
+                  type="text"
+                  placeholder="Senha"
+                  autoComplete="off"
+                  fullWidth
+                  size="small"
+                />
+              </Grid>
+
+              <Grid container item lg={6} sm={12} xs={12} spacing={2}>
+                <Grid item lg={6} md={12} sm={12} xs={12}>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    disableElevation
+                    disabled={isLoading}
+                    sx={{
+                      width: '100%',
+                      marginBottom: '10px',
+                      [theme.breakpoints.down('lg')]: { marginBottom: '0px' },
+                    }}
+                    endIcon={
+                      isLoading ? (
+                        <CircularProgress
+                          variant="indeterminate"
+                          color="inherit"
+                          size={20}
+                          sx={{ alignSelf: 'end' }}
+                        />
+                      ) : undefined
+                    }
+                  >
+                    {isLoading ? 'Enviando...' : 'Enviar Chamado'}
+                  </Button>
+                  {/* <Snackbar
+                    open={openSuccessMessage}
+                    autoHideDuration={6000}
+                    onClose={handleClose}
+                    anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                  >
+                    <Alert severity="success" onClose={handleClose}>
+                      Usuário ou senha incorretos
+                    </Alert>
+                  </Snackbar> */}
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
-
-          <VTextField name="author" placeholder="Autor" fullWidth />
-
-          <VTextField name="publishedAt" placeholder="publishedAt" fullWidth />
-        </VForm> */}
+        </form>
       </Box>
     </DefaultLayout>
   )
