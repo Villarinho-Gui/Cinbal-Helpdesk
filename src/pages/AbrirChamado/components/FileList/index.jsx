e4a1f75e406b0d929290f6d5e1b92e80f038e482
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-05-11 17:23:34 -0300

    feat(pages\abrirchamado\components\filelist\index.jsx): :sparkles: Alterando layout

--- src/pages/AbrirChamado/components/FileList/index.jsx
+++ src/pages/AbrirChamado/components/FileList/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { MdDelete, MdImage } from 'react-icons/md'
-import { FileListContainer, FileInfo } from './styles'
-import { Button } from '@mui/material'
+import { Button, Card, Typography, Box } from '@mui/material'
 import { filesize } from 'filesize'
 
 export function FileList({ image, onDeleteImage }) {
@@ -11,28 +10,55 @@ export function FileList({ image, onDeleteImage }) {
   }
 
   return (
-    <FileListContainer id="lista-arquivos">
-      <li>
-        <FileInfo>
-          {image ? (
-            <img
-              src={URL.createObjectURL(image)}
-              alt=""
-              height={40}
-              width={40}
-            />
-          ) : (
-            <img src={<MdImage size={35} />} alt="" />
-          )}
-          <div>
-            <strong>{image.name}</strong>
-            <span>{filesize(image.size)}</span>
-          </div>
-        </FileInfo>
-        <Button color="error" onClick={triggerDeleteImage}>
-          <MdDelete size={25} />
-        </Button>
-      </li>
-    </FileListContainer>
+    <Card
+      component={Box}
+      overflow={'hidden'}
+      display={'flex'}
+      alignItems={'center'}
+      width={'350px'}
+      justifyContent={'space-between'}
+      color="#444"
+      padding={'0.5rem'}
+      borderRadius={'8px'}
+      variant="outlined"
+    >
+      <Box display={'flex'} alignItems={'center'} gap={'1rem'}>
+        {image ? (
+          <img src={URL.createObjectURL(image)} alt="" height={40} width={40} />
+        ) : (
+          <img src={<MdImage size={35} />} alt="" />
+        )}
+        <Box
+          display={'flex'}
+          width={'200px'}
+          justifyContent={'center'}
+          flexDirection={'column'}
+        >
+          <Box
+            maxWidth={'30ch'}
+            whiteSpace={'nowrap'}
+            textOverflow={'ellipsis'}
+            overflow={'hidden'}
+          >
+            <Typography variant="h6" sx={{ fontSize: '16px' }}>
+              {image.name}
+            </Typography>
+          </Box>
+          <Box
+            fontSize={'0.75rem'}
+            marginTop={'4px'}
+            maxWidth={'30ch'}
+            width={'70%'}
+          >
+            <Typography variant="body2" fontSize={'12px'} color="#999">
+              {filesize(image.size)}
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+      <Button color="error" onClick={triggerDeleteImage}>
+        <MdDelete size={25} />
+      </Button>
+    </Card>
   )
 }
