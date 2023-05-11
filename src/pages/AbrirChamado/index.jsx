051c728a4886ddfb0b3b63aa92ef4762a895a37c
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-05-11 17:08:37 -0300

    feat(pages\abrirchamado\index.jsx): :sparkles: Reimplementação do formulário
    Ainda serão feitas novas alterações

--- src/pages/AbrirChamado/index.jsx
+++ src/pages/AbrirChamado/index.jsx
@@ -3,29 +3,20 @@ import api from '../../service/api/config/configApi'
 import { FileList } from './components/FileList'
 import { uniqueId } from 'lodash'
 
-import { Button, Autocomplete, Box, TextField } from '@mui/material'
-import { AbrirChamadoContainer, Title } from './styles'
-
-const categorias = [
-  { label: 'Email' },
-  { label: 'Telefone' },
-  { label: 'Rede' },
-  { label: 'Fluig' },
-  { label: 'Hardware' },
-  { label: 'Software' },
-  { label: 'PC FACTORY' },
-  {
-    label: 'PREACTOR',
-  },
-  { label: 'PROTHEUS' },
-  { label: 'VEXON' },
-  {
-    label: 'Portal do Cliente',
-  },
-  {
-    label: 'Outros',
-  },
-]
+import {
+  Button,
+  Box,
+  TextField,
+  Grid,
+  useTheme,
+  IconButton,
+  Tooltip,
+  Divider,
+  Select,
+  MenuItem,
+} from '@mui/material'
+import DefaultLayout from '../../shared/layouts/DefaultLayout'
+import { AiOutlinePaperClip } from 'react-icons/ai'
 
 export default function AbrirChamado() {
   const [image, setImage] = useState('')
@@ -39,15 +30,20 @@ export default function AbrirChamado() {
 
   const [titulo, setTitulo] = useState('')
   const [descricao, setDescricao] = useState('')
+  const [categoria, setCategoria] = useState('Email')
+
+  const handleChange = (event) => {
+    setCategoria(event.target.value)
+  }
 
   const PostChamado = async (e) => {
     e.preventDefault()
 
-    console.log('Upload')
     const formData = new FormData()
     formData.append('image', image)
     formData.append('titulo', titulo)
     formData.append('descricao', descricao)
+    formData.append('categoria', categoria)
 
     for (let i = 0; i < image.length; i++) {
       formData.append('image', image[i])
@@ -97,112 +93,184 @@ export default function AbrirChamado() {
     })
 
     setImage(newListImageWithoutDeletedOne)
-
-    // console.log(`Deletar ${image}`);
   }
 
+  const theme = useTheme()
+
   return (
-    <>
-      <AbrirChamadoContainer>
-        <Title>Abrir Chamado</Title>
-
-        <form className="AbrirChamadoForm" onSubmit={PostChamado} method="post">
-          {status.type === 'success' ? (
-            <p>{status.mensagem}</p>
-          ) : (
-            <p>{status.mensagem}</p>
-          )}
-          <div className="formPrincipais">
-            <Autocomplete
-              id="combo-box-demo"
-              name="categoria"
-              disablePortal
-              options={categorias}
-              sx={{ width: '100%' }}
-              renderInput={(params) => (
-                <TextField {...params} label="Categoria" />
-              )}
-            />
-            <Button
-              className="upload"
-              variant="contained"
-              component="label"
-              color="primary"
-              sx={{ width: '100%', maxHeight: '56px' }}
-              onChange={triggerSelectNewFile}
+    <DefaultLayout
+      tituloPagina="Abrir Chamado"
+      mostrarBotaoLogout
+      mostrarBotaoPerfil
+      mostrarBotaoTema
+    >
+      <Box
+        borderRadius={1}
+        marginX={1}
+        width="auto"
+        border="1px solid"
+        height="max-content"
+        borderColor={theme.palette.divider}
+      >
+        <Grid
+          container
+          direction="column"
+          padding={5}
+          spacing={2}
+          sx={{
+            [theme.breakpoints.down('sm')]: {
+              padding: '30px',
+            },
+          }}
+        >
+          <form
+            className="AbrirChamadoForm"
+            onSubmit={PostChamado}
+            method="post"
+          >
+            {status.type === 'success' ? (
+              <p>{status.mensagem}</p>
+            ) : (
+              <p>{status.mensagem}</p>
+            )}
+            <Grid
+              container
+              item
+              direction={'column'}
+              spacing={2}
+              sx={{ maxWidth: '600px' }}
             >
-              Anexar arquivo
-              <input
-                name="file"
-                value={newUploadImage}
-                hidden
-                accept="image/*"
-                multiple
-                type="file"
-                onChange={(e) => {
-                  setImage([...image, ...e.target.files])
-                  triggerNewImageChange()
-                }}
-              />
-            </Button>
-          </div>
-
-          {image && image.length > 0 && (
-            <div className="FileList">
-              {image.map((file) => {
-                return (
-                  <FileList
-                    image={file}
-                    key={uniqueId(file.id)}
-                    onDeleteImage={deleteImage}
+              <Grid item xl={4} lg={6}>
+                <Box sx={{ minWidth: 120, paddingTop: 3 }}>
+                  <TextField
+                    id="titulo_id"
+                    name="titulo"
+                    label="Título"
+                    variant="outlined"
+                    value={titulo}
+                    onChange={(e) => setTitulo(e.target.value)}
+                    sx={{ width: '100%' }}
                   />
-                )
-              })}
-            </div>
-          )}
+                </Box>
+              </Grid>
+              <Grid item xl={4}>
+                <Select
+                  id="combo-box-demo"
+                  name="categoria"
+                  value={categoria}
+                  onChange={handleChange}
+                  sx={{ width: '100%' }}
+                >
+                  <MenuItem value={'email'}>Email</MenuItem>
+                  <MenuItem value={'ramal'}>Ramal</MenuItem>
+                  <MenuItem value={'rede'}>Rede</MenuItem>
+                  <MenuItem value={'fluig'}>Fluig</MenuItem>
+                  <MenuItem value={'hardware'}>Hardware</MenuItem>
+                  <MenuItem value={'software'}>Software</MenuItem>
+                  <MenuItem value={'pcfactory'}>PcFactory</MenuItem>
+                  <MenuItem value={'preactor'}>Preactor</MenuItem>
+                  <MenuItem value={'protheus'}>Protheus</MenuItem>
+                  <MenuItem value={'vexon'}>Vexon</MenuItem>
+                  <MenuItem value={'portaldocliente'}>
+                    Portal do Cliente
+                  </MenuItem>
+                  <MenuItem value={'outros'}>Outros</MenuItem>
+                </Select>
+              </Grid>
+            </Grid>
 
-          <Box sx={{ minWidth: 120, paddingTop: 3 }}>
-            <TextField
-              id="titulo_id"
-              name="titulo"
-              label="Título"
-              variant="outlined"
-              value={titulo}
-              onChange={(e) => setTitulo(e.target.value)}
-              sx={{ width: '100%' }}
-            />
-          </Box>
-          <Box sx={{ minWidth: 120, paddingTop: 3 }}>
-            <TextField
-              id="descricao_id"
-              name="descricao"
-              label="Descrição"
-              variant="outlined"
-              multiline
-              rows={4}
-              sx={{ width: '100%' }}
-              value={descricao}
-              onChange={(e) => setDescricao(e.target.value)}
-            />
-          </Box>
-          <div className="ActionButtons">
-            <Button
-              type="button"
-              variant="outlined"
-              sx={{ width: '100%', maxHeight: '56px' }}
+            <Grid item xl={6} sx={{ marginY: '20px' }}>
+              <TextField
+                id="descricao_id"
+                name="descricao"
+                label="Descrição"
+                variant="outlined"
+                multiline
+                rows={4}
+                sx={{ width: '100%' }}
+                value={descricao}
+                onChange={(e) => setDescricao(e.target.value)}
+              />
+            </Grid>
+
+            <Grid
+              container
+              spacing={2}
+              direction={'row'}
+              alignItems={'center'}
+              gap={2}
             >
-              Voltar
-            </Button>
-            <Button
-              type="submit"
-              variant="contained"
-              sx={{ width: '100%', maxHeight: '56px' }}
+              {image && image.length > 0 && (
+                <Grid
+                  container
+                  gap={2}
+                  paddingY={4}
+                  paddingX={2}
+                  className="FileList"
+                >
+                  {image.map((file) => {
+                    return (
+                      <FileList
+                        image={file}
+                        key={uniqueId(file.id)}
+                        onDeleteImage={deleteImage}
+                      />
+                    )
+                  })}
+                </Grid>
+              )}
+            </Grid>
+
+            <Grid
+              container
+              direction={'row'}
+              spacing={2}
+              paddingY={2}
+              alignItems={'center'}
             >
-              Abrir Chamado
-            </Button>
-          </div>
-        </form>
-      </AbrirChamadoContainer>
-    </>
+              <Grid item>
+                <Tooltip title="Anexar arquivo" placement="top" arrow>
+                  <IconButton
+                    className="upload"
+                    variant="contained"
+                    component="label"
+                    color="primary"
+                    onChange={triggerSelectNewFile}
+                  >
+                    <input
+                      name="file"
+                      value={newUploadImage}
+                      hidden
+                      accept="image/*"
+                      multiple
+                      type="file"
+                      onChange={(e) => {
+                        setImage([...image, ...e.target.files])
+                        triggerNewImageChange()
+                      }}
+                    />
+                    <AiOutlinePaperClip size={25} />
+                  </IconButton>
+                </Tooltip>
+              </Grid>
+              <Divider
+                variant="middle"
+                orientation="vertical"
+                sx={{ height: '35px', marginTop: '20px' }}
+              />
+              <Grid item xl={2} lg={6}>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  sx={{ width: '100%' }}
+                >
+                  Enviar chamado
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </Grid>
+      </Box>
+    </DefaultLayout>
   )
 }
