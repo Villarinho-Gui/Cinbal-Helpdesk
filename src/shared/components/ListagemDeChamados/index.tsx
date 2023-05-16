6d47d239ea34dc475fd6fc590c74eb2022e015e8
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-05-16 16:07:53 -0300

    refactor(listagemdechamados): :dizzy: Refazendo a listagem de chamados
    Precisa concertar alguns erros e também ajustar a pesquisa de chamados que deixou de funcionar.

--- src/shared/components/ListagemDeChamados/index.tsx
+++ src/shared/components/ListagemDeChamados/index.tsx
@@ -1,50 +1,58 @@
 /* eslint-disable react/jsx-key */
-import React, { useMemo } from 'react'
+import React, { useMemo, useState, useEffect } from 'react'
 import DefaultLayout from '../../layouts/DefaultLayout'
-// import { Chamado } from '../Chamado'
+import { Chamado } from '../Chamado'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 
-// import { useDebounce } from '../../hooks/UseDebounce'
-// import { Alert, LinearProgress, List, ListItem } from '@mui/material'
-// import { Environment } from '../../environment/export'
+import { useDebounce } from '../../hooks/UseDebounce'
+import { Alert, LinearProgress, List, ListItem } from '@mui/material'
+import { Environment } from '../../environment/export'
 
-// import { VscError } from 'react-icons/vsc'
+import { VscError } from 'react-icons/vsc'
 import { BarraFerramentasListagemDeChamados } from '../BarraFerramentasListagemDeChamados'
+
+import api from '../../../service/api/config/configApi'
 
 export const ListagemDeChamados: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams()
 
-  // const { debounce } = useDebounce()
+  const { debounce } = useDebounce()
 
   const navigate = useNavigate()
 
-  // const [chamados, setChamados] = useState([])
-  // const [totalCount, setTotalCount] = useState<number>(0)
-  // const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [chamados, setChamados] = useState([])
+  const [totalCount, setTotalCount] = useState<number>(0)
+  const [isLoading, setIsLoading] = useState<boolean>(true)
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || ''
   }, [searchParams])
 
-  // const pagina = useMemo(() => {
-  //   return Number(searchParams.get('pagina') || '1')
-  // }, [searchParams])
+  const pagina = useMemo(() => {
+    return Number(searchParams.get('pagina') || '1')
+  }, [searchParams])
 
-  // useEffect(() => {
-  //   setIsLoading(true)
-  //   debounce(() => {
-  //     ChamadosService.getAll(pagina, busca).then(() => {
-  //       setIsLoading(false)
-  //       if (result instanceof Error) {
-  //         alert(result.message)
-  //       } else {
-  //         console.log(result)
-  //         setChamados(result.data)
-  //         setTotalCount(Number(result.totalCount))
-  //       }
-  //     })
-  //   })
-  // }, [busca, debounce, pagina])
+  useEffect(() => {
+    setIsLoading(true)
+    debounce(async () => {
+      await api
+        .get('/chamados', {
+          params: { pagina, busca },
+        })
+        .then((response) => {
+          const { data } = response
+          data.createdAt = new Date(data.createdAt)
+          setIsLoading(false)
+          console.log(data)
+          setChamados(data)
+          setTotalCount(Number(data.totalCount))
+        })
+        .catch((error) => {
+          console.log(error)
+          alert('Ocorreu um erro ao buscar os chamados')
+        })
+    })
+  }, [busca, debounce, pagina])
 
   return (
     <DefaultLayout
@@ -65,21 +73,23 @@ export const ListagemDeChamados: React.FC = () => {
         />
       }
     >
-      {/* <List sx={{ overflow: 'auto', padding: '0px' }}>
+      <List sx={{ overflow: 'auto', padding: '0px' }}>
         {chamados.map((chamado) => (
-          <ListItem key={chamado.id} disablePadding>
+          <ListItem disablePadding>
             <Chamado
-              titulo={chamado.titulo}
-              descricao={chamado.descricao}
               id={chamado.id}
-              author={chamado.author}
-              publishedAt={chamado.publishedAt}
+              author={''}
+              titulo={''}
+              categoria={''}
+              descricao={''}
+              maxLines={0}
+              createdAt={null}
             />
           </ListItem>
         ))}
-      </List> */}
+      </List>
 
-      {/* {totalCount === 0 && !isLoading && (
+      {totalCount === 0 && !isLoading && (
         <Alert
           variant="standard"
           color="error"
@@ -88,11 +98,11 @@ export const ListagemDeChamados: React.FC = () => {
         >
           {Environment.LISTAGEM_VAZIA}
         </Alert>
-      )} */}
+      )}
 
-      {/* {isLoading && (
+      {isLoading && (
         <LinearProgress variant="indeterminate" sx={{ marginX: '10px' }} />
-      )} */}
+      )}
     </DefaultLayout>
   )
 }
