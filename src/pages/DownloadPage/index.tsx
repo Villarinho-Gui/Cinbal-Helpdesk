9376c706e01228cbeadc51f5dea0461667b182c3
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-05-18 07:56:05 -0300

    feat (downloadpage) :sparkles: Adicionando botão de retorno

--- src/pages/DownloadPage/index.tsx
+++ src/pages/DownloadPage/index.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import api from '../../service/api/config/configApi'
 import DefaultLayout from '../../shared/layouts/DefaultLayout'
-import { Box, Button } from '@mui/material'
+import { Box, Button, IconButton, Tooltip, Typography } from '@mui/material'
 import { BsDownload } from 'react-icons/bs'
+import { AiOutlineArrowLeft } from 'react-icons/ai'
 
 const DownloadPage = () => {
   const { filename } = useParams()
   const [fileUrl, setFileUrl] = useState('')
+  const navigate = useNavigate()
 
   useEffect(() => {
     const fetchFileUrl = async () => {
@@ -26,9 +28,33 @@ const DownloadPage = () => {
     fetchFileUrl()
   }, [filename])
 
+  /**
+   * Função para retornar para a página anterior, ou seja, o chamado correspondente.
+   */
+
+  const triggerReturnChamado = () => {
+    navigate(-1)
+  }
+
   return (
-    <DefaultLayout tituloPagina={'Download do arquivo'}>
-      <Box marginX={'10px'}>
+    <DefaultLayout
+      tituloPagina={'Download do arquivo'}
+      mostrarBotaoLogout
+      mostrarBotaoTema
+      mostrarBotaoPerfil
+    >
+      <Box
+        marginX={'10px'}
+        display={'flex'}
+        width={'400px'}
+        gap={'5'}
+        justifyContent={'space-between'}
+      >
+        <Tooltip title="Voltar para o chamado" placement="top" arrow>
+          <IconButton onClick={triggerReturnChamado}>
+            <AiOutlineArrowLeft />
+          </IconButton>
+        </Tooltip>
         {fileUrl ? (
           <a href={fileUrl} download={`${filename}.jpg`}>
             <Button variant="contained" endIcon={<BsDownload size={15} />}>
@@ -36,7 +62,9 @@ const DownloadPage = () => {
             </Button>
           </a>
         ) : (
-          <p>Carregando...</p>
+          <Typography variant="h6" color={'3b8eef'}>
+            Carregando...
+          </Typography>
         )}
       </Box>
     </DefaultLayout>
