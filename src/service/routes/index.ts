9e008ada2968f975d50994b29068a0ea2a2ddc21
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-05-26 11:17:49 -0300

    feat(src\service\routes): :sparkles: Rota /abrir-chamado
    Foi adicionado o multer de arquivos

--- src/service/routes/index.ts
+++ src/service/routes/index.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express'
 import { UserController } from '../controllers/UserController'
+import { HelpDeskController } from '../controllers/HelpDeskController'
+import { uploadHelpDesk } from '../middlewares/uploadFilesMiddleware'
 
 const router = Router()
 
 router.post('/cadastro', new UserController().create)
 router.post('/login', new UserController().login)
+
+router.post(
+  '/abrir-chamado',
+  uploadHelpDesk.array('files'),
+  new HelpDeskController().create,
+)
 
 export { router }
