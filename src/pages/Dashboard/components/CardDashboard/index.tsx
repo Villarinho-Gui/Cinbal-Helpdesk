a2445fba48e511a12ec6068634c5ca973882257b
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-06-05 07:18:36 -0300

    feat (Card Dashboard) :sparkles: Componente de card

---
+++ src/pages/Dashboard/components/CardDashboard/index.tsx
@@ -0,0 +1,92 @@
+import {
+  Card,
+  Box,
+  CardContent,
+  Typography,
+  Skeleton,
+  Button,
+  useTheme,
+} from '@mui/material'
+import React, { useState, useEffect } from 'react'
+
+import api from '../../../../service/api/config/configApi'
+
+interface FileProps {
+  id: string
+  url: string
+  callId: string
+}
+interface HelpDeskDashboardProps {
+  id: string
+  author: string
+  title: string
+  category: string
+  description: string
+  files?: FileProps[]
+  createdAt: Date
+}
+
+export const CardDashboard: React.FC = () => {
+  const [isLoadingChamados, setIsLoadingChamados] = useState<boolean>(false)
+  const [helpDeskData, setHelpDeskData] = useState<HelpDeskDashboardProps[]>([])
+  const theme = useTheme()
+
+  useEffect(() => {
+    setIsLoadingChamados(true)
+
+    api.get('/chamados').then((response) => {
+      const { data } = response
+
+      setIsLoadingChamados(false)
+      if (response instanceof Error) {
+        alert(response.message)
+      } else {
+        setHelpDeskData(Object.values(data)[0] as HelpDeskDashboardProps[])
+      }
+    })
+  }, [])
+
+  return (
+    <Card
+      elevation={0}
+      padding={2}
+      variant="outlined"
+      component={Box}
+      borderRadius={1}
+      marginX={2}
+      border="1px solid"
+      borderColor={theme.palette.divider}
+    >
+      <CardContent>
+        <Box>
+          <Typography
+            variant="h5"
+            sx={{ fontSize: '1rem' }}
+            color="text.secondary"
+          >
+            Chamados Abertos
+          </Typography>
+          <Box display="flex" gap={2} width="100%" alignItems="center">
+            {isLoadingChamados ? (
+              <Skeleton
+                variant="text"
+                sx={{ fontSize: '4rem' }}
+                width="100px"
+              />
+            ) : (
+              <Typography
+                variant="body2"
+                color={theme.palette.text.primary}
+                sx={{ fontSize: '3rem' }}
+              >
+                {helpDeskData.length}
+              </Typography>
+            )}
+          </Box>
+
+          <Button>Visualizar</Button>
+        </Box>
+      </CardContent>
+    </Card>
+  )
+}
