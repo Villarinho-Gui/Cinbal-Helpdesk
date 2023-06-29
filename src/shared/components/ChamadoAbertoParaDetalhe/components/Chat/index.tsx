55a92f3b6e95646f48804e5987d85cb66e789117
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-06-29 14:54:09 -0300

    feat(chat): :sparkles: Adicionando TextField e mensagem
    Ainda não funcional

--- src/shared/components/ChamadoAbertoParaDetalhe/components/Chat/index.tsx
+++ src/shared/components/ChamadoAbertoParaDetalhe/components/Chat/index.tsx
@@ -1,17 +1,35 @@
+import React, { useState } from 'react'
 import { Box, useTheme } from '@mui/material'
-import React from 'react'
+import { MessageTextField } from './components/MessageTextField'
+import { MessageComponent } from './components/MessageComponent'
 
-const Chat: React.FC = () => {
+// interface SendMessageListProps {
+//   user: {
+//     name: string
+//   }
+//   createdAt: Date
+//   content: string
+//   files?: File[]
+// }
+
+export const Chat: React.FC = () => {
+  // const [sendMessage, setSendMessage] = useState('')
+  // const [textFieldText, setTextFieldText] = useState('')
   const theme = useTheme()
+
+  // Função de envio
+
   return (
-    <Box
-      bgcolor={theme.palette.background.default}
-      display={'flex'}
-      height={250}
-    >
-      Interaction Here
-    </Box>
+    <>
+      <Box
+        bgcolor={theme.palette.background.default}
+        display={'flex'}
+        justifyContent={'end'}
+        height={250}
+      >
+        <MessageComponent />
+      </Box>
+      <MessageTextField />
+    </>
   )
 }
-
-export default Chat
