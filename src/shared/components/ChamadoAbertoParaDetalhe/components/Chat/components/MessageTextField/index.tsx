32a645347ee780e067fabc045b3e5f835cd8f944
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-07-03 11:31:15 -0300

    feat(messagetextfield): :sparkles: Enviando mensagem
    Acessando rota POST

--- src/shared/components/ChamadoAbertoParaDetalhe/components/Chat/components/MessageTextField/index.tsx
+++ src/shared/components/ChamadoAbertoParaDetalhe/components/Chat/components/MessageTextField/index.tsx
@@ -1,20 +1,52 @@
-import React from 'react'
-import {
-  Box,
-  Icon,
-  IconButton,
-  TextField,
-  Tooltip,
-  useTheme,
-} from '@mui/material'
+import React, { useState } from 'react'
+import { Icon, IconButton, TextField, Tooltip, useTheme } from '@mui/material'
 import { AiOutlinePaperClip } from 'react-icons/ai'
 import { IoMdSend } from 'react-icons/io'
-import { FileList } from '../../../../../../../pages/AbrirChamado/components/FileList'
+// import { FileList } from '../../../../../../../pages/AbrirChamado/components/FileList'
+import api from '../../../../../../../service/api/config/configApi'
+import { useForm } from 'react-hook-form'
+import { useParams } from 'react-router-dom'
+import { useHelpDeskContext } from '../../../../../../contexts/HelpDeskContext'
 
-// import { Container } from './styles';
+interface SendMessageProps {
+  messageContent: string
+  helpdeskId: string
+  files?: File[]
+}
 
 export const MessageTextField: React.FC = () => {
+  const [textFieldMessage, setTextFieldMessage] = useState('')
+
+  const { id } = useParams()
   const theme = useTheme()
+  const { toggleMessage } = useHelpDeskContext()
+  const { handleSubmit, register } = useForm()
+
+  const PostMessage = async () => {
+    const token = localStorage.getItem('access_token')
+
+    const formData = new FormData()
+
+    formData.append('message', textFieldMessage)
+
+    const headers = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `bearer ${token}`,
+      },
+    }
+
+    try {
+      await api
+        .post<SendMessageProps>(`/coment/${id}`, formData, headers)
+        .then(() => {
+          toggleMessage()
+        })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <>
       {/* <FileList
@@ -23,18 +55,26 @@ export const MessageTextField: React.FC = () => {
           throw new Error('Function not implemented.')
         }}
       /> */}
-      <Box marginY={2} display={'flex'} gap={2}>
-        <form style={{ width: '100%' }}>
-          <TextField
-            fullWidth
-            size={'small'}
-            maxRows={5}
-            multiline
-            sx={{
-              bgcolor: [theme.palette.background.default],
-            }}
-          />
-        </form>
+      <form
+        method="POST"
+        onSubmit={handleSubmit(PostMessage)}
+        style={{ width: '100%', display: 'flex', marginTop: '10px' }}
+      >
+        <TextField
+          fullWidth
+          {...register('message')}
+          name="message"
+          placeholder="Escreva uma mensagem..."
+          size={'small'}
+          type="text"
+          maxRows={5}
+          multiline
+          value={textFieldMessage}
+          onChange={(e) => setTextFieldMessage(e.target.value)}
+          sx={{
+            bgcolor: [theme.palette.background.default],
+          }}
+        />
         <Tooltip title="Anexar arquivo" placement="top" arrow>
           <IconButton className="upload" component="label" color="primary">
             <input
@@ -47,12 +87,12 @@ export const MessageTextField: React.FC = () => {
             <AiOutlinePaperClip size={25} />
           </IconButton>
         </Tooltip>
-        <IconButton>
+        <IconButton type="submit">
           <Icon>
             <IoMdSend size={20} />
           </Icon>
         </IconButton>
-      </Box>
+      </form>
     </>
   )
 }
