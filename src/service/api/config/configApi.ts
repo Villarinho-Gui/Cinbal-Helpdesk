580546b0e96420beda2abd1a57e111c1b7d97843
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-07-03 13:22:22 -0300

    refactor(api\config): :dizzy: Atualizando link da api

--- src/service/api/config/configApi.ts
+++ src/service/api/config/configApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://10.152.6.107:3000',
+  baseURL: 'http://10.152.6.108:3000',
   // baseURL: 'http://localhost:8181',
 })
