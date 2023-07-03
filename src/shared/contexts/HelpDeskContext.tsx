98c19b515bc094362b747717dc1cfc617b45581e
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-07-03 11:25:46 -0300

    feat(helpdeskcontext.tsx): :sparkles: Gerenciamento de estado para novas mensagens

--- src/shared/contexts/HelpDeskContext.tsx
+++ src/shared/contexts/HelpDeskContext.tsx
@@ -3,6 +3,8 @@ import React, { createContext, useCallback, useContext, useState } from 'react'
 interface HelpDeskContextProps {
   toggleHelpDesk: () => void
   isNewHelpDesk: true | false
+  toggleMessage: () => void
+  isNewMessage: true | false
   toggleLoading: () => void
   isLoading: true | false
 }
@@ -21,10 +23,15 @@ export const HelpDeskProvider: React.FC<HelpDeskContextChildren> = ({
   children,
 }) => {
   const [isNewHelpDesk, setIsNewHelpDesk] = useState(false)
+  const [isNewMessage, setIsNewMessage] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
 
   const toggleHelpDesk = useCallback(() => {
     setIsNewHelpDesk((oldHelpDesk) => !oldHelpDesk)
+  }, [])
+
+  const toggleMessage = useCallback(() => {
+    setIsNewMessage((oldMessage) => !oldMessage)
   }, [])
 
   const toggleLoading = useCallback(() => {
@@ -33,7 +40,14 @@ export const HelpDeskProvider: React.FC<HelpDeskContextChildren> = ({
 
   return (
     <HelpDeskContext.Provider
-      value={{ toggleHelpDesk, isNewHelpDesk, toggleLoading, isLoading }}
+      value={{
+        toggleHelpDesk,
+        isNewHelpDesk,
+        toggleLoading,
+        isLoading,
+        toggleMessage,
+        isNewMessage,
+      }}
     >
       {children}
     </HelpDeskContext.Provider>
