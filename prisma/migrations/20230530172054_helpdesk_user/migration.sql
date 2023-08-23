f61f863122f9744e3a0d61b23f49460fda9adc53
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-08-23 14:02:28 -0300

    docs: :open_file_folder: Removendo desnecessidades

--- prisma/migrations/20230530172054_helpdesk_user/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `createdAt` on the `call` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `DateTime`.
-
-*/
--- AlterTable
-ALTER TABLE `call` MODIFY `createdAt` DATETIME NOT NULL;
