0407810b4e4286a86fd0862faa448bf251d1c6d6
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-09-06 14:25:39 -0300

    feat: (MessageTextField) :sparkles: adicionando FileList

--- src/shared/components/ChamadoAbertoParaDetalhe/components/Chat/components/MessageTextField/index.tsx
+++ src/shared/components/ChamadoAbertoParaDetalhe/components/Chat/components/MessageTextField/index.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 import {
   Box,
+  Card,
   CircularProgress,
   Icon,
   IconButton,
+  List,
+  ListItem,
   TextField,
   Tooltip,
   useTheme,
@@ -14,6 +17,8 @@ import api from '../../../../../../../service/api/config/configApi'
 import { useForm } from 'react-hook-form'
 import { useParams } from 'react-router-dom'
 import { useHelpDeskContext } from '../../../../../../contexts/HelpDeskContext'
+import FileList from '../../../../../../../pages/AbrirChamado/components/FileList'
+import { uniqueId } from 'lodash'
 export interface SendMessageProps {
   messageContent: string
   helpdeskId: string
@@ -22,6 +27,8 @@ export interface SendMessageProps {
 
 export const MessageTextField: React.FC = () => {
   const [textFieldMessage, setTextFieldMessage] = useState('')
+  const [attachedFiles, setAttachedFiles] = useState<File[] | undefined>([])
+  const [newUploadFile, setNewUploadFile] = useState<File | undefined>()
 
   const { id } = useParams()
   const theme = useTheme()
@@ -60,61 +67,113 @@ export const MessageTextField: React.FC = () => {
     }
   }
 
+  function triggerNewImageChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const file = event.currentTarget.files![0]
+    setNewUploadFile(file)
+  }
+
+  function triggerSelectNewFile() {
+    if (newUploadFile !== undefined) {
+      setAttachedFiles([...attachedFiles!, newUploadFile!])
+    }
+
+    setNewUploadFile(undefined)
+  }
+
+  const deleteFile = (attachedFileToDelete: any) => {
+    const newListImageWithoutDeletedOne = attachedFiles!.filter((file) => {
+      return file !== attachedFileToDelete
+    })
+
+    setAttachedFiles(newListImageWithoutDeletedOne)
+  }
+
   return (
-    <Box display={'flex'} alignItems={'flex-start'} justifyContent={'center'}>
-      {/* <FileList
-        file={undefined}
-        onDeleteFile={function (attachedFileToDelete: any): void {
-          throw new Error('Function not implemented.')
-        }}
-      /> */}
-      <form
-        id="messageTextField"
-        method="POST"
-        onSubmit={handleSubmit(PostMessage)}
-        style={{ width: '100%', display: 'flex', marginTop: '10px' }}
-      >
-        <TextField
-          fullWidth
-          {...register('message')}
-          name="message"
-          placeholder="Escreva uma mensagem..."
-          size={'small'}
-          type="text"
-          maxRows={5}
-          multiline
-          value={textFieldMessage}
-          onChange={(e) => setTextFieldMessage(e.target.value)}
-          sx={{
-            bgcolor: [theme.palette.background.default],
-          }}
-          disabled={isLoading}
-        />
-      </form>
-      <Box display={'flex'} marginTop={1} marginX={1}>
-        <Tooltip title="Anexar arquivo" placement="top" arrow>
-          <IconButton className="upload" component="label" color="primary">
-            <input
-              id="file-input"
-              hidden
-              accept="image/*"
-              type="file"
-              multiple
-              form="messageTextField"
-            />
-            <AiOutlinePaperClip size={25} />
+    <>
+      {attachedFiles && attachedFiles.length > 0 && (
+        <Box borderRadius={'8px'} component={Card} elevation={0} padding={1}>
+          <List
+            sx={{
+              overflow: 'auto',
+              display: 'flex',
+            }}
+          >
+            {attachedFiles.map((file) => {
+              return (
+                <ListItem
+                  key={uniqueId(String(file.lastModified))}
+                  sx={{
+                    padding: '0',
+                    marginX: '4px',
+                    width: 'max-content',
+                  }}
+                >
+                  <FileList file={file} onDeleteFile={deleteFile} />
+                </ListItem>
+              )
+            })}
+          </List>
+        </Box>
+      )}
+
+      <Box display={'flex'} alignItems={'flex-start'} justifyContent={'center'}>
+        <form
+          id="messageTextField"
+          method="POST"
+          onSubmit={handleSubmit(PostMessage)}
+          style={{ width: '100%', display: 'flex', marginTop: '10px' }}
+        >
+          <TextField
+            fullWidth
+            {...register('message')}
+            name="message"
+            placeholder="Escreva uma mensagem..."
+            size={'small'}
+            type="text"
+            maxRows={5}
+            multiline
+            value={textFieldMessage}
+            onChange={(e) => setTextFieldMessage(e.target.value)}
+            sx={{
+              bgcolor: [theme.palette.background.default],
+            }}
+            disabled={isLoading}
+          />
+        </form>
+        <Box display={'flex'} marginTop={1} marginX={1}>
+          <Tooltip title="Anexar arquivo" placement="top" arrow>
+            <IconButton
+              className="upload"
+              component="label"
+              color="primary"
+              onChange={triggerSelectNewFile}
+            >
+              <input
+                {...register('files')}
+                id="file-input"
+                hidden
+                type="file"
+                multiple
+                disabled={isLoading}
+                onChange={(e) => {
+                  setAttachedFiles([...attachedFiles!, ...e.target.files!])
+                  triggerNewImageChange(e)
+                }}
+              />
+              <AiOutlinePaperClip size={25} />
+            </IconButton>
+          </Tooltip>
+          <IconButton type="submit" disabled={isLoading} onClick={PostMessage}>
+            <Icon>
+              {isLoading ? (
+                <CircularProgress size={25} />
+              ) : (
+                <IoMdSend size={20} />
+              )}
+            </Icon>
           </IconButton>
-        </Tooltip>
-        <IconButton type="submit" disabled={isLoading} onClick={PostMessage}>
-          <Icon>
-            {isLoading ? (
-              <CircularProgress size={25} />
-            ) : (
-              <IoMdSend size={20} />
-            )}
-          </Icon>
-        </IconButton>
+        </Box>
       </Box>
-    </Box>
+    </>
   )
 }
