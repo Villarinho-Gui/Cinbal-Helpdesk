1d77cf66a5d2b79f4d04b335dad584c812d105fe
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-11-10 15:25:06 -0300

    Novo host

--- src/service/api/config/configApi.ts
+++ src/service/api/config/configApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://10.152.7.151:3545',
+  baseURL: 'http://192.168.56.1:3545',
   // baseURL: 'http://localhost:8181',
 })
