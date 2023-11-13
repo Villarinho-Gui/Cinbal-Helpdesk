0099062d53ada5cc2fcbb974b9ec553f575e5b8c
guilherme.villarinho <Guivillarinho15@gmail.com>
2023-11-13 09:37:17 -0300

    removendo o s do link

--- src/service/api/config/configApi.ts
+++ src/service/api/config/configApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'https://10.152.7.151:3545',
+  baseURL: 'http://10.152.7.151:3545',
   // baseURL: 'http://localhost:8181',
 })
