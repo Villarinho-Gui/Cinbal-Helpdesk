c8468c0ae1e2284c1b9f0bcbb7054b27f710cd1d
Guilherme Villarinho <Guivillarinho15@gmail.com>
2024-01-08 17:03:47 -0300

    feat: :sparkles: Adicionando mutate do react-query

---
+++ src/shared/hooks/usePostHelpDeskMutate/index.ts
@@ -0,0 +1,17 @@
+import { useMutation, useQueryClient } from 'react-query'
+import api from '../../../service/api'
+
+const submit = async (data: any) => {
+  return await api.post('/helpdesk', data)
+}
+
+export function usePostHelpDeskMutate() {
+  const queryClient = useQueryClient()
+  const mutate = useMutation({
+    mutationFn: submit,
+    onSuccess: () => {
+      queryClient.invalidateQueries(`helpDesk-data`)
+    },
+  })
+  return mutate
+}
