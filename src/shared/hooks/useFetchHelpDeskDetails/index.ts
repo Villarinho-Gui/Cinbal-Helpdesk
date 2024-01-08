69026e6f70d77543179c8736ef62a7ac21e4c18c
Guilherme Villarinho <Guivillarinho15@gmail.com>
2024-01-08 17:03:17 -0300

    alterando o nome

---
+++ src/shared/hooks/useFetchHelpDeskDetails/index.ts
@@ -0,0 +1,31 @@
+import api from '../../../service/api'
+import { useQuery } from 'react-query'
+import { HelpDeskProps } from '../../types/helpdeskType'
+import { useParams } from 'react-router-dom'
+
+export function useFetchHelpDeskDetails(url: string) {
+  const { id } = useParams()
+
+  const token = localStorage.getItem('access_token')
+  const headers = {
+    headers: {
+      Authorization: `bearer ${token}`,
+    },
+  }
+
+  const fetchData = async () => {
+    const { data } = await api.get<HelpDeskProps>(
+      `http://apihd.cinbal.com.br/${url}`,
+      headers,
+    )
+    return data
+  }
+
+  const query = useQuery({
+    queryKey: ['helpDesk-data', id],
+    queryFn: fetchData,
+    refetchOnWindowFocus: false,
+  })
+
+  return query
+}
